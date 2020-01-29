7c59669d2cf62e1c6eb00fda2e32680ef9a9fc1a
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-01-29 20:26:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Carvices
+ Android app that helps users find car repair or towing services near their location.
---
+++ app/src/main/java/com/vickikbt/carvices/Registerctivity.kt
@@ -0,0 +1,12 @@
+package com.vickikbt.carvices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registerctivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registerctivity)
+    }
+}
