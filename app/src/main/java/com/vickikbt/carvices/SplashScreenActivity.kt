709f4600505fd8528e583761dd6fdfcefafea42c
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-01-29 20:35:45 +0300

    Added delay in the splashscreen.

--- app/src/main/java/com/vickikbt/carvices/SplashScreenActivity.kt
+++ app/src/main/java/com/vickikbt/carvices/SplashScreenActivity.kt
@@ -1,12 +1,18 @@
 package com.vickikbt.carvices
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+        }, 2200)
     }
 }
