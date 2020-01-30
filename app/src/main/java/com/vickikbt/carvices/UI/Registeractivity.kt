7c584824c507fd815430f6bfc21789d77b98a235
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-01-30 21:59:18 +0300

    Finished activity_register.xml layout.

--- app/src/main/java/com/vickikbt/carvices/UI/Registeractivity.kt
+++ app/src/main/java/com/vickikbt/carvices/UI/Registeractivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vickikbt.carvices.R
 
-class Registerctivity : AppCompatActivity() {
+class Registeractivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
