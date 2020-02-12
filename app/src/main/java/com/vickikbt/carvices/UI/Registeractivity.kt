7e94927f44b5ae0b279394cf73df6bfe5fa7590f
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-12 19:52:42 +0300

    Added data binding to RegisterActivity

--- app/src/main/java/com/vickikbt/carvices/UI/Registeractivity.kt
+++ app/src/main/java/com/vickikbt/carvices/UI/Registeractivity.kt
@@ -1,13 +1,17 @@
 package com.vickikbt.carvices.UI
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.vickikbt.carvices.R
+import com.vickikbt.carvices.databinding.ActivityRegisteractivityBinding
 
 class Registeractivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityRegisteractivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registeractivity)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_registeractivity)
     }
 }
