72916f13b9381a323443806e8fc4fc5b3bef5468
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-12 20:09:12 +0300

    Finished UI and Logic code in LoginActivity

--- app/src/main/java/com/vickikbt/carvices/ui/LoginActivity.kt
+++ app/src/main/java/com/vickikbt/carvices/ui/LoginActivity.kt
@@ -1,22 +1,64 @@
 package com.vickikbt.carvices.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.vickikbt.carvices.R
 import com.vickikbt.carvices.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityLoginBinding
+    lateinit var binding: ActivityLoginBinding
+    val firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-        binding.buttonLogin.setOnClickListener(){
+        binding.buttonLogin.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        binding.buttonLogin.setOnClickListener {
+            loginUser()
+        }
     }
+
+    private fun loginUser() {
+        val email = binding.editTextEmailLogin.text.toString()
+        val password = binding.editTextPasswordLogin.text.toString()
+
+        when {
+            email.isEmpty() -> Toast.makeText(this, "Enter Email!", Toast.LENGTH_SHORT).show()
+            password.isEmpty() -> Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show()
+        }
+
+        if (email.isEmpty() || password.isEmpty()) {
+            return
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to login because: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
 }
