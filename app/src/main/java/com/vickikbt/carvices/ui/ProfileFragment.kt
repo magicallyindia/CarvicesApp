617541aea7e3729289734419f3053ef4dcece1b6
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-12 20:32:38 +0300

    Added databinding to the ProfileFragment

--- app/src/main/java/com/vickikbt/carvices/ui/ProfileFragment.kt
+++ app/src/main/java/com/vickikbt/carvices/ui/ProfileFragment.kt
@@ -2,23 +2,25 @@ package com.vickikbt.carvices.ui
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.vickikbt.carvices.R
+import com.vickikbt.carvices.databinding.FragmentProfileBinding
 
-/**
- * A simple [Fragment] subclass.
- */
 class ProfileFragment : Fragment() {
+
+    lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+        return binding.root
     }
 
 
